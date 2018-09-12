b817bb75c26b78d96099f3d415cf24f7822e9d42
yuanwansong <yuanwansong@hotmail.com>
2018-09-12 19:56:05 +0800

    init

--- config/server.config.js
+++ config/server.config.js
@@ -1,16 +1,4 @@
 let ServerConfig = {
-    //  见微信开发文档：https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842
-    //  微信开发者测试号申请：https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
-    weixin: {
-        //  微信公众号开发ID
-        appID: '',
-        //  微信公众号开发秘钥
-        appSecret: '',
-        //  页面授权类型
-        scope: 'snsapi_userinfo',
-        //  页面授权后的回掉地址
-        callBackUrl: ''
-    },
     //  服务器启动端口
     serverPort: 3000,
     //  数据库配置
