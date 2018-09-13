83211430a3a7d6958b2c0e273fe48c5ae5531d46
yuanwansong <yuanwansong@hotmail.com>
2018-09-13 20:20:06 +0800

    init

--- src/config/global.js
+++ src/config/global.js
@@ -1,15 +1,15 @@
 module.exports = {
     wx: {
-        appID: 'wxd9f715f33cbff819',
-        appSecret: '5092c8b91064471983e5cdcbfad817e6'
+        appID: '',
+        appSecret: ''
     },
     serverPort: 81,
     mysql: {
-        host: '120.77.45.3',
-        user: 'u_wedding',
-        password: 'yws930708',
-        port: '3306',
-        database: 'wedding'
+        host: '',
+        user: '',
+        password: '',
+        port: '',
+        database: ''
     },
     debugMode: true,
     //  每位用户每天最大抽奖次数限制
